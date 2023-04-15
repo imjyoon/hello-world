1147ee22c279dc1dbc9f3c3fc36a8e532f761a10
wldbs <ljy5459@gmail.com>
2023-04-15 11:31:13 +0900

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("Hello World");
+        System.out.println("Merci");
+        System.out.print("Three...");
+        System.out.print("Two...");
+        System.out.println("One...");
+        System.out.println("Let's go out together!");
+    }
+}
